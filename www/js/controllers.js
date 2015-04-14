e5e9fc157035abc96c053653a30c6f79ef2307df
Micha Kops <micha.kops@gmail.com>
2015-04-14 10:58:36 +0200

    Basic search layout added.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,11 @@
 angular.module('hascodeTagWizard.controllers', [])
-.controller('SearchController', function ($scope) {
+.controller('SearchController', function ($scope, searchService) {
+    $scope.term = '';
+    $scope.hits = [];
+    $scope.hasSearched = false;
 
+    $scope.searchTerm = function(){
+       $scope.hasSearched = true;
+       $scope.hits = searchService.searchByTag($scope.term) || [];
+    };
 });
--- www/js/services.js
+++ www/js/services.js
@@ -1,11 +1,11 @@
 angular.module('hascodeTagWizard.services', [])
 
-.factory('SearchService', function() {
+.factory('searchService', function() {
 
   var articles = [
-      {title:'Foo'},
-      {title:'Bar'},
-      {title:'Xoxo'}
+      {title:'Foo', url:'http://www.hascode.com/xxx', image:'http://www.hascode.com/wp-content/uploads/2011/04/ocpjp1.png', excerpt:'When it comes to indexing in a Neo4j graph database, different options exist for a developer to create and maintain the index. In the following short examples I’d like to demonstrate different possibilities for index management.'},
+      {title:'Bar', url:'http://www.hascode.com/xxx', image:'http://www.hascode.com/wp-content/uploads/2011/04/ocpjp1.png', excerpt:'When it comes to indexing in a Neo4j graph database, different options exist for a developer to create and maintain the index. In the following short examples I’d like to demonstrate different possibilities for index management.'},,
+      {title:'Baz', url:'http://www.hascode.com/xxx', image:'http://www.hascode.com/wp-content/uploads/2011/04/ocpjp1.png', excerpt:'When it comes to indexing in a Neo4j graph database, different options exist for a developer to create and maintain the index. In the following short examples I’d like to demonstrate different possibilities for index management.'},
   ];
 
   return {
